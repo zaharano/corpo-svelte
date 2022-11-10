e8a6f307e68a18b859b51835c131bcf4f80bac65
Zachary Rais-Norman <zraisnorman@gmail.com>
2022-11-10 14:20:18 -0500

    start custom typewriter component

---
+++ src/lib/TypeOut.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let content = 'This is the default text content for the TypingOut component.';
+
+  // split into array and make it all spans
+</script>
+
+<style>
+  span::before {
+    content: ''
+  }
+</style>
