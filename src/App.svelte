68437f9a1e937f3645e40b47b1ace402e533a49c
Zachary Rais-Norman <zraisnorman@gmail.com>
2022-11-29 16:12:05 -0500

    move bladerunner screen to component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Screen from './Components/Screen.svelte'
   import TypeOut from './lib/TypeOut.svelte';
+  import Runner from './lib/Runner.svelte';
 </script>
 
 <main>
@@ -8,25 +9,8 @@
     <div class='screen-container'>
       <Screen />
       <div class='screen-type-container'>
-        <!-- <div class='screen-type-title'>ENVIRON CTR</div>
-        *************************** <br />-
-        &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
-        &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
-        &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
-        &nbsp;&nbsp;92886599 | 95654085&nbsp;&nbsp; - <br /> -
-        &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
-        &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
-        &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
-        &nbsp;&nbsp;###################&nbsp;&nbsp; - <br />
-        ***************************
-        <div class='screen-type-title'>
-          24556&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; DR&nbsp;&nbsp;&nbsp; 5
-        </div>
-        &nbsp; -->
-        <TypeOut content="It's your first day in the mail room at Megacorp! Thanks to your shady Uncle Harrison, Director of South American Regional Logistics, you've been given your shot at the corporate world. The mail room isn't the best shot, seeing as it's the year 2085 and nobody sends mail any more, but it's the one you've been given so damnit you're gonna give it your all! Your manager Merle has been at Megacorp for 30 years, and is eager to show you the ropes." typeSpeed={4} />
-        <TypeOut content="It's your first day in the mail room at Megacorp! Thanks to your shady Uncle Harrison, Director of South American Regional Logistics, you've been given your shot at the corporate world. The mail room isn't the best shot, seeing as it's the year 2085 and nobody sends mail any more, but it's the one you've been given so damnit you're gonna give it your all! Your manager Merle has been at Megacorp for 30 years, and is eager to show you the ropes." typeSpeed={4} />
-        
-
+        <!-- <TypeOut content="It's your first day in the mail room at Megacorp! Thanks to your shady Uncle Harrison, Director of South American Regional Logistics, you've been given your shot at the corporate world. The mail room isn't the best shot, seeing as it's the year 2085 and nobody sends mail any more, but it's the one you've been given so damnit you're gonna give it your all! Your manager Merle has been at Megacorp for 30 years, and is eager to show you the ropes." typeSpeed={4} /> -->
+        <Runner />
       </div>
     </div>
   </div>
@@ -52,16 +36,9 @@
   }
   .screen-type-container {
     position: absolute;
-    top: 19%;
-    left: 18%;
+    top: 18%;
+    left: 17.5%;
     width: 65%;
     height: 60%;
   }
-
-  /* .screen-type-title {
-    font-family: Rubik;
-    font-size: 7vmin;
-    font-weight: 600;
-  } */
-
 </style>
---
+++ src/lib/Runner.svelte
@@ -0,0 +1,37 @@
+
+<div class='screen-type'>
+  <div class='screen-type-title'>ENVIRON CTR</div>
+  *************************** <br />-
+  &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
+  &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
+  &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
+  &nbsp;&nbsp;92886599 | 95654085&nbsp;&nbsp; - <br /> -
+  &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
+  &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
+  &nbsp;&nbsp;###################&nbsp;&nbsp; - <br /> -
+  &nbsp;&nbsp;###################&nbsp;&nbsp; - <br />
+  ***************************
+  <div class='screen-type-title'>
+    24556&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; DR&nbsp;&nbsp;&nbsp; 5
+  </div>
+  &nbsp;
+</div>
+
+<style>
+  .screen-type {
+    font-family: 'Rubik Mono One', Courier, monospace;
+    font-weight: 400;
+    font-size: 2.5vmin;
+    text-align: center;
+    color: #8fe6e2;
+    text-shadow: 0px 0px 9.12281px rgba(73, 193, 188, 0.8),
+      0px 0px 54.7368px #49c1bc;
+    filter: blur(0.07em);
+  }
+
+  .screen-type-title {
+    font-family: Rubik;
+    font-size: 7vmin;
+    font-weight: 600;
+  }
+</style>
