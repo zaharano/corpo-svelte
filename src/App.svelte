e880ef15dac3b34a44846fe8878b3adf4935af10
Zachary Rais-Norman <zraisnorman@gmail.com>
2022-12-07 12:34:52 -0500

    component structure, add fx

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,21 @@
 <script>
-  import Screen from './Components/Screen.svelte'
   import TypeOut from './lib/TypeOut.svelte';
-  import Runner from './lib/Runner.svelte';
+  import Runner from './lib/screens/Runner.svelte';
+  import Megacorp from './lib/screens/Megacorp.svelte';
+  import ScreenHousing from './lib/ScreenHousing.svelte';
+  import ScreenFx from './lib/screens/ScreenFx.svelte';
+  import TextContent from './lib/screens/TextContent.svelte';
 </script>
 
 <main>
   <div class="app-container">
     <div class='screen-container'>
-      <Screen />
-      <div class='screen-type-container'>
-        <!-- <TypeOut content="It's your first day in the mail room at Megacorp! Thanks to your shady Uncle Harrison, Director of South American Regional Logistics, you've been given your shot at the corporate world. The mail room isn't the best shot, seeing as it's the year 2085 and nobody sends mail any more, but it's the one you've been given so damnit you're gonna give it your all! Your manager Merle has been at Megacorp for 30 years, and is eager to show you the ropes." typeSpeed={4} /> -->
-        <Runner />
-      </div>
+      <ScreenHousing />
+      <ScreenFx>
+        <TextContent slot='content' >
+          <TypeOut slot='text'  />
+        </TextContent>
+      </ScreenFx>
     </div>
   </div>
 </main>
@@ -30,15 +34,10 @@
     font-size: calc(10px + 2vmin);
     color: white;
   }
+
   .screen-container {
     width: 100vmin;
     position: absolute;
   }
-  .screen-type-container {
-    position: absolute;
-    top: 18%;
-    left: 17.5%;
-    width: 65%;
-    height: 60%;
-  }
+
 </style>
---
+++ src/lib/screens/TextContent.svelte
@@ -0,0 +1,13 @@
+<div class='screen-type-container'>
+  <slot name='text'>No defined text</slot>
+</div>
+
+<style>
+  .screen-type-container {
+    position: absolute;
+    top: 18%;
+    left: 17.5%;
+    width: 65%;
+    height: 60%;
+  }
+</style>
