94f10fad35e42648e30ac9a6bbd4d07648983970
Zachary Rais-Norman <zraisnorman@gmail.com>
2022-12-08 16:13:56 -0500

    options and css vars

---
+++ src/lib/Option.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let text;
+  export let num;
+  
+</script>
+
+<button class='option'>
+  <span class="option-num">{num}.</span><span class="option-text">{text}</span>
+</button>
+
+<style>
+  .option {
+    padding: .7em;
+    background-color: inherit;
+    border: none;
+    color: inherit;
+    display: block;
+    text-shadow: inherit;
+    width: 100%;
+    text-align: inherit;
+    display: flex;
+    flex-direction: row;
+    font-family: var(--primary-font);
+    font-size: inherit;
+  }
+
+  .option:hover {
+    background-color: var(--text);
+    box-shadow: var(--box-glow);
+    color: var(--inversion);
+  }
+
+  .option-num {
+    margin-right: 1em;
+    font-weight: 700;
+    font-size: 110%;
+  }
+</style>
---
+++ src/lib/Options.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Option from "./Option.svelte";
+
+  export let options = [
+    {
+      text: "Release this sucker and start raking in the accolades!",
+    },
+    {
+      text: "Compromises? No. This is my magnum opus, my gift to humankind. It will be perfect or it will be nothing.",
+    }
+  ]
+</script>
+
+<div>
+  {#each options as option, idx}
+   <Option num={idx} {...option}/>
+  {/each}
+</div>
+
+
+<style>
+  div {
+   margin: .5em 0;
+  }
+</style>
