5a0f64d0e4047b5457f49da2a09aa45ca6b3f1c7
Zachary Rais-Norman <zraisnorman@gmail.com>
2022-12-20 15:52:25 -0500

    classing up timer.js

--- src/help/timer.js
+++ src/help/timer.js
@@ -1,51 +1,52 @@
 // timer constructor
 // instantiates a timer that can be started and stopped and keeps an aggregate
 // TODO add optional param of a cached DOM ref and handle interval internally
-// TODO move methods to prototype? who cares? just make it a class already?
 
-function Timer(name) {
-  this.name = name;
-  this.initTime = Date.now();
-  this.startTime = undefined;
-  this.totalTime = 0;
-  this.isRunning = false;
+class Timer {
+  constructor(name) {
+    this.name = name;
+    this.initTime = Date.now();
+    this.startTime = undefined;
+    this.totalTime = 0;
+    this.isRunning = false;
+  }
 
-  this.start = function () {
+  start() {
     if (this.isRunning) {
       return console.warn('Timer is already running');
     }
     this.isRunning = true;
     this.startTime = Date.now();
-  };
+  }
 
-  this.stop = function () {
+  stop() {
     if (!this.isRunning) {
       return console.warn('Timer is already stopped');
     }
     this.totalTime = this.totalTime + this.elapsed();
     this.isRunning = false;
-  };
+  }
 
-  this.elapsed = function () {
+  elapsed() {
     if (!this.isRunning) {
       return 0;
     }
     return Date.now() - this.startTime;
-  };
+  }
 
-  this.reset = function () {
+  reset() {
     this.totalTime = 0;
     this.isRunning = false;
     this.startTime = undefined;
-  };
+  }
 
-  this.time = function () {
+  time() {
     // not yet run case
     if (this.isRunning) {
       return this.totalTime + this.elapsed();
     }
     return this.totalTime;
-  };
+  }
 }
 
 export default Timer;
