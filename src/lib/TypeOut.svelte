aa6615f3581af29a04332f65ba6b9255984ea14d
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-01-30 15:42:10 -0500

    make typeout ready for changing text

--- src/lib/TypeOut.svelte
+++ src/lib/TypeOut.svelte
@@ -1,16 +1,17 @@
 <script>
-  import {onMount} from 'svelte';
+  import { onMount } from 'svelte'
+  import { textLoaded, text } from '../help/stores'
 
   export let content = 'This is the default text content for the TypingOut component.';
   export let typeSpeed = 50;
 	
-  let cArr = content.split("");
-  let domArr = [];
 	let container;
-  let timer = null;
-  let clear;
 
-  function seq() {
+  function newText() {
+    clear()
+    let cArr = content.split("")
+    let timer = null
+
     let added = 0;
     let revealed = 0;
     
@@ -29,13 +30,19 @@
         revealed++;
       }
       else {
+        setTimeout(() => {
+          textLoaded.set(true)
+        }, 400);
         clearInterval(timer);
       }
     }, typeSpeed);
   }
-  // TODO: make interval slightly random?
-  // TODO: fix for line breaks emerging as text is added
-  // make append happen immediately but cache the array of characters as they are added. Then use the interval to change classes to make opaque and animate
+
+  function clear() {
+    while(container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+  }
 
   function addCharacter(c, count) {
     const child = document.createElement('span');
@@ -47,41 +54,30 @@
 
   function revealCharacter(count) {
     const selected = document.querySelector(`#c${count}`)
-    selected.classList.add('burn')
     selected.classList.remove('hidden')
+    selected.classList.add('burnt')
   }
 
 	onMount(() => {
-    seq();
-    // seq will need to fire on any change of the text.
-    clear = function() {
-      while(container.hasChildNodes()) {
-        container.removeChild(container.firstChild);
-    }
-  }
+    newText();
 	});
 
 </script>
 
-<div class='typeOut-container' bind:this={container} on:click={clear()}/>
+<div class='typeOut-container' bind:this={container} />
 
 <style>
-  :global(.burn) {
+  .typeOut-container {
     color: hsl(177, 64%, 95%);
-    text-shadow: 0px 0px 9.12281px white, 0px 0px 54.7368px white;
-    animation: burn 600ms linear 1 forwards;
+    text-shadow: 0px 0px 9.12281px hsl(0, 0%, 100%), 0px 0px 54.7368px hsl(0, 0%, 100%);
+    /* filter: drop-shadow(0 0 55px white) */
   }
-  
-  @keyframes burn {
-    0% {
-      color: hsl(177, 64%, 95%);
-      text-shadow: 0px 0px 9.12281px white, 0px 0px 54.7368px white;
-    }
-    to {
-      color: hsl(177, 62%, 63%);
-      text-shadow: 0px 0px 9.12281px rgba(73, 193, 188, 0.8),
-        0px 0px 54.7368px #49c1bc;
-    }
+
+  :global(.burnt) {
+    color: hsl(177, 62%, 63%);
+    text-shadow: 0px 0px 9.12281px hsla(178, 49%, 52%, 0.8), 0px 0px 54.7368px hsl(178, 49%, 52%);
+    /* filter: drop-shadow(0 0 55px hsl(178, 49%, 52%)); */
+    transition: color 1s, text-shadow 1s;
   }
 
   :global(.hidden) {
