039469f75a6b6c8d5f44ff083a44bf5faa3a0101
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-01-30 15:21:59 -0500

    unused things

--- src/lib/screens/Display.svelte
+++ src/lib/screens/Display.svelte
@@ -1,17 +1,19 @@
 <script>
-  import MainDisplay from "./MainDisplay.svelte";
-  import TypeOut from "../TypeOut.svelte";
-  import Options from "../Options.svelte";
-  import Header from "./Header.svelte";
+  import TypeOut from "../TypeOut.svelte"
+  import Options from "../Options.svelte"
+  import Header from "./Header.svelte"
   import Footer from "./Footer.svelte"
+
+  export let content = "You're the Vice President of Nanotechnology at Megacorp! You're a VP. After years of fastidious boot-licking, you're in upper management. The vacation package alone makes it all worth while. After one year in the role, you're forced out because they're 'taking the company in another direction.'"
+  export let typeSpeed = 20
 </script>
 
 <div>
   <Header />
-  <MainDisplay>
-    <TypeOut slot='text' content="You're the Vice President of Nanotechnology at Megacorp! You're a VP. After years of fastidious boot-licking, you're in upper management. The vacation package alone makes it all worth while. After one year in the role, you're forced out because they're 'taking the company in another direction.' Seems you took a little too long to get here. But with the severance you buy an island. Enjoy retirement! This is just to write a little more and maybe make this go a bit past the bottom and we can see how it goes!" typeSpeed={20} />
-    <Options slot='options' />
-  </MainDisplay>
+  <main>
+    <TypeOut {content} {typeSpeed} />
+    <Options />
+  </main>
   <Footer />
 </div>
 
@@ -28,5 +30,12 @@
     display: flex;
     flex-direction: column;
     height: 100%;
+  }
+
+  main {
+    overflow: scroll;
+    scroll-behavior: smooth;
+    padding-top: .5em;
+    flex-grow: 2;
   }
 </style>
