325e00c41829915099fa23aad34e0639ece8e95c
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-01-30 15:22:25 -0500

    start title screen setup

--- src/lib/screens/Megacorp.svelte
+++ src/lib/screens/Megacorp.svelte
@@ -1,4 +1,4 @@
-<div>
+<div class='container'>
   <pre>
 ███╗   ███╗███████╗ ██████╗  █████╗  ██████╗ ██████╗ ██████╗ ██████╗ 
 ████╗ ████║██╔════╝██╔════╝ ██╔══██╗██╔════╝██╔═══██╗██╔══██╗██╔══██╗
@@ -8,12 +8,15 @@
 ╚═╝     ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     
                                                              
   </pre>
-  WE LOVE WORK
+  <ul class='menu'>
+    <li>PUNCH IN</li>
+    <li>OPTIONS</li>
+  </ul>
 </div>
 
 
 <style>
-  div {
+  .container {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -22,6 +25,7 @@
     transform: scale(1.2);
     color: hsl(177, 62%, 63%);
   }
+
   pre {
     font-size: calc(3px + 1vmin);
     color: hsl(177, 62%, 63%);
@@ -29,5 +33,21 @@
       0px 0px 5em hsla(178, 49%, 52%, 0.6);
     filter: blur(0.1em);
     opacity: .9;
+  } 
+
+  ul {
+    list-style-type: none;
+    margin: 0;
   }
+
+  li {
+    padding: .5em;
+    color: var(--text);
+  }
+
+  li:hover {
+    background-color: var(--text);
+    color: var(--inversion);
+  }
+
 </style>
