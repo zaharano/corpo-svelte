cd0cf8f4efe95ec263eb70900dd217b5a036a0fb
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-02-03 11:43:43 -0500

    add management to Display (add full control)

--- src/lib/screens/Display.svelte
+++ src/lib/screens/Display.svelte
@@ -3,18 +3,26 @@
   import Options from "../Options.svelte"
   import Header from "./Header.svelte"
   import Footer from "./Footer.svelte"
+  import Popup from "../Popup.svelte"
 
-  export let content = "You're the Vice President of Nanotechnology at Megacorp! You're a VP. After years of fastidious boot-licking, you're in upper management. The vacation package alone makes it all worth while. After one year in the role, you're forced out because they're 'taking the company in another direction.'"
-  export let typeSpeed = 20
+  import { text, options, currentEvent, currentScreen } from '../help/stores'
+
+  // TODO: figure out what to do with the scrollbar styling
+  // TODO: the prop passing (which is useless) is causing the repaint so we need to figure out how to do without
+  function cycleDisplay() {
+    text.set($currentEvent[$currentScreen].text);
+    options.set($currentEvent[$currentScreen].opts);
+  }
 </script>
 
 <div>
   <Header />
   <main>
-    <TypeOut {content} {typeSpeed} />
-    <Options />
+    <TypeOut {$text} />
+    <Options {options} {cycleDisplay}/>
   </main>
   <Footer />
+  <Popup />
 </div>
 
 <style>
@@ -22,7 +30,6 @@
     text-align: left;
     line-height: 1.2;
     font-family: var(--primary-font);
-    /* font-weight: 800; */
     font-size: 2.3vmin;
     color: var(--text);
     text-shadow: var(--text-glow);
@@ -33,9 +40,19 @@
   }
 
   main {
-    overflow: scroll;
+    overflow-y: scroll;
     scroll-behavior: smooth;
     padding-top: .5em;
     flex-grow: 2;
+    scrollbar-color: var(--text) rgba(0, 0, 0, 0);
+  }
+
+  main::-webkit-scrollbar-track {
+    background: rgba(0, 0, 0, 0);
+  }
+
+  main::-webkit-scrollbar-thumb {
+    background-color: var(--text);
+    border-radius: 0px;
   }
 </style>
