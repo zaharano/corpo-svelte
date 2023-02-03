4d70eb1fa085c8a51557265fe49b90cb6869ab3b
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-02-03 12:24:19 -0500

    fix TypeOut anim sequence, set up for wipe + new

--- src/lib/TypeOut.svelte
+++ src/lib/TypeOut.svelte
@@ -1,46 +1,17 @@
 <script>
   import { onMount } from 'svelte'
-  import { textLoaded, text } from '../help/stores'
+  import { textLoaded, effects, text } from './help/stores'
 
-  export let content = 'This is the default text content for the TypingOut component.';
-  export let typeSpeed = 50;
-	
+	$: $text, newText();
+  
 	let container;
 
-  function newText() {
-    clear()
-    let cArr = content.split("")
-    let timer = null
-
-    let added = 0;
-    let revealed = 0;
-    
-    for (let i = 0; i < cArr.length; i++) {
-      addCharacter(cArr[added], added)
-      added++;
-    }
-
-    timer = setInterval(() => {
-      let prev = document.querySelector('.burn');
-      if (prev) {
-        prev.classList.remove('burn');
-      }
-      if (revealed < cArr.length) {
-        revealCharacter(revealed)
-        revealed++;
-      }
-      else {
-        setTimeout(() => {
-          textLoaded.set(true)
-        }, 400);
-        clearInterval(timer);
-      }
-    }, typeSpeed);
-  }
+  // couldn't find out other way set up the reactive func only active after mount
+  let newText = () => { };
 
   function clear() {
     while(container.hasChildNodes()) {
-        container.removeChild(container.firstChild);
+      container.removeChild(container.firstChild);
     }
   }
 
@@ -49,16 +20,49 @@
     child.id = `c${count}`
     child.textContent = c;
     child.classList.add('hidden');
+    child.classList.add('burning');
     container.appendChild(child);
   }
 
   function revealCharacter(count) {
     const selected = document.querySelector(`#c${count}`)
     selected.classList.remove('hidden')
-    selected.classList.add('burnt')
+    selected.classList.remove('burning')
   }
 
 	onMount(() => {
+    newText = () => {
+      textLoaded.set(false)
+      clear()
+      let content = $text;
+      let cArr = content.split("")
+      let timer = null
+
+      let added = 0;
+      let revealed = 0;
+      
+      for (let i = 0; i < cArr.length; i++) {
+        addCharacter(cArr[added], added)
+        added++;
+      }
+
+      timer = setInterval(() => {
+        let prev = document.querySelector('.burn');
+        if (prev) {
+          prev.classList.remove('burn');
+        }
+        if (revealed < cArr.length) {
+          revealCharacter(revealed)
+          revealed++;
+        }
+        else {
+          setTimeout(() => {
+            textLoaded.set(true)
+          }, 500);
+          clearInterval(timer);
+        }
+      }, $effects.typeSpeed);
+    }
     newText();
 	});
 
@@ -68,16 +72,18 @@
 
 <style>
   .typeOut-container {
-    color: hsl(177, 64%, 95%);
-    text-shadow: 0px 0px 9.12281px hsl(0, 0%, 100%), 0px 0px 54.7368px hsl(0, 0%, 100%);
+    transition: color 1s, text-shadow 1s;
     /* filter: drop-shadow(0 0 55px white) */
   }
 
-  :global(.burnt) {
-    color: hsl(177, 62%, 63%);
-    text-shadow: 0px 0px 9.12281px hsla(178, 49%, 52%, 0.8), 0px 0px 54.7368px hsl(178, 49%, 52%);
+  .typeOut-container :global(span) {
+    transition: color 1.2s, text-shadow 1.2s;
+  }
+
+  .typeOut-container :global(.burning) {
+    color: hsl(177, 64%, 95%);
+    text-shadow: 0px 0px 9.12281px hsl(0, 0%, 100%), 0px 0px 54.7368px hsl(0, 0%, 100%);
     /* filter: drop-shadow(0 0 55px hsl(178, 49%, 52%)); */
-    transition: color 1s, text-shadow 1s;
   }
 
   :global(.hidden) {
