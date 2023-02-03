d199e5975aef33b8b3130eb308fdd6d150e1846b
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-02-03 11:42:29 -0500

    start popup (will need bg reconfig)

---
+++ src/lib/Popup.svelte
@@ -0,0 +1,38 @@
+<div class="popup-container">
+  <div class="popup">
+    this is some conceptual popup text. testing to see where this goes and I think it's a propblem
+    <button>OK</button>
+  </div>
+</div>
+
+<style>
+  .popup-container {
+    position: absolute;
+    height: 100%;
+    width: 80%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-left: 10%;
+  }
+  .popup {
+    background-color: black;
+    border: 3px solid var(--text);
+    box-shadow: var(--text-glow), var(--text-glow) inset;
+    margin: auto;
+    padding: 20px 30px;
+    display: flex;
+    flex-direction: column;
+  }
+  button {
+    display: block;
+    box-shadow: var(--line-glow), var(--line-glow) inset;
+    background-color: rgba(0,0,0,0);
+    color: var(--text);
+    border: 3px solid var(--text);
+    padding: 3px 10px;
+    font-size: inherit;
+    font-family: inherit;
+    margin-top: 1em;
+  }
+</style>
