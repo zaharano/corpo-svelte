e1a31280c53a9ebe01671b234a567a3d84d93714
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-02-08 16:44:35 -0500

    Component-ify button

---
+++ src/lib/Button.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let clickHandler;
+  export let text;
+</script>
+
+<button on:click={clickHandler}>{text}</button>
+
+<style>
+  button {
+    display: block;
+    box-shadow: var(--line-glow), var(--line-glow) inset;
+    background-color: rgba(0,0,0,0);
+    color: var(--text);
+    border: 3px solid var(--text);
+    padding: 3px 10px;
+    font-size: inherit;
+    font-family: inherit;
+    margin-top: 1em;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: var(--text);
+    box-shadow: var(--box-glow);
+    color: var(--inversion);
+  }
+</style>
