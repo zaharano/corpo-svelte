10c1ad03907ed2574d62ca0f14363516a0f028e6
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-03-14 16:50:58 -0400

    vitest config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  test: {
+    // ...
+  },
+  plugins: [svelte()],
+});
