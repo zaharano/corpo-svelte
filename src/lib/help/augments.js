37325ac391c841a30681d35a6b8febdd1a87fa8a
Zachary Rais-Norman <zraisnorman@gmail.com>
2023-03-14 16:52:10 -0400

    augments constant

---
+++ src/lib/help/augments.js
@@ -0,0 +1,9 @@
+const potentialAugments = [
+  'Adrenaline Pump',
+  'Blood Oxygen Recycler',
+  'Central Core Gyroscope',
+  'Dermal Calcification',
+  'Toxicity Neutralizing Nanobots',
+  'Optical Spectrum Enhancement',
+  'Emergency Datajack Breaker',
+];
