396caf43f45b532a303572966ed22bc199612cf2
Zach <zraisnorman@gmail.com>
2023-09-21 00:21:52 -0400

    add header readout

--- src/lib/screens/Header.svelte
+++ src/lib/screens/Header.svelte
@@ -1,17 +1,30 @@
 <script>
+  import { job } from '../help/stores'
 
 </script>
 
-<div>
-  (c) MEGACORP
+<div class="readout">
+  <div class='title'><strong>{$job.title}</strong> in <strong>{$job.department}</strong></div>
+  <div class='years'>Years of Service: {$job.years}</div>
 </div>
 
 <style>
-  div {
+  .readout {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
     border-bottom: 2px solid var(--text);
     width: 100%;
     text-align: center;
     padding-bottom: .3em;
+    line-height: 1.1;
+    font-size: .7em;
+  }
+  .title {
+    text-align: left;
+  }
+  .years {
+    text-align: right;
   }
 
 
