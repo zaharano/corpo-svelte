936cc5cdfb2bbed3c84fcee049713bfffb8bc752
Zach <zraisnorman@gmail.com>
2023-09-20 23:02:30 -0400

    enhance tracker

--- src/lib/Tracker.svelte
+++ src/lib/Tracker.svelte
@@ -8,6 +8,11 @@
   let workTimeDisplay;
   let idleTimeDisplay;
   let idleTracker;
+  let absentTracker;
+  let delinquentTracker;
+  let idle = false;
+  let absent = false;
+  let delinquent = false;
 
   let timeDisplayInterval;
   let idleDisplayInterval;
@@ -26,16 +31,25 @@
     resetIdle();
   }
 
+  // TODO: I should debounce this, it fires a lot
   function resetIdle() {
     if (inactiveTimer.isRunning) {
       inactiveTimer.stop();
     }
     clearTimeout(idleTracker);
-    idleTracker = setTimeout(inactive, 2000);
+    clearTimeout(absentTracker);
+    clearTimeout(delinquentTracker);
+    idleTracker = setTimeout(setIdle, 5000);
+    absentTracker = setTimeout(() => absent = true, 30000);
+    delinquentTracker = setTimeout(() => delinquent = true, 60000);
+    idle = false;
+    absent = false;
+    delinquent = false;
   }
 
-  function inactive() {
+  function setIdle() {
     inactiveTimer.start();
+    idle = true;
   }
 
   onMount(() => {
@@ -50,16 +64,61 @@
   on:click={resetIdle}
 />
 
-<span><strong>TIME: </strong>{workTimeDisplay} <strong>IDLE: </strong>{idleTimeDisplay}</span>
+<div class="stats error">
+  <div class="times">
+    <div><strong>Time in: </strong>{workTimeDisplay}</div>
+    <div><strong>Time idle: </strong>{idleTimeDisplay}</div>
+  </div>
+  <div class={`status ${delinquent ? 'delinquent-text' : absent ? 'error-text' : idle ? 'warning-text' : 'good-text'}`}>
+    <div><strong>Status: </strong></div>
+    <div>{delinquent ? 'Delinquent' : absent ? 'Absent' : idle ? 'Idle' : 'Working'}</div>
+  </div>
+  <div class="ratio">
+    <strong>Ratio: </strong>{(idleTimeDisplay / workTimeDisplay).toFixed(2)}
+  </div>  
+</div>
+
 
 <style>
-  span {
-    text-align: center;
+  .stats {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
     width: 100%;
     line-height: 1.1;
+    font-size: .7em;
+  }
+  .error-text {
+    color: hsl(23, 100%, 50%);
+    text-shadow: 0px 0px 9.12281px hsla(23, 100%, 50%, .8),
+      0px 0px 54.7368px hsla(23, 100%, 50%, 1);
+  }
+  .warning-text {
+    color: hsla(60, 100%, 50%, 1);
+    text-shadow: 0px 0px 9.12281px hsla(60, 100%, 50%, .8),
+      0px 0px 54.7368px hsla(60, 100%, 50%, 1);
+  }
+  .good-text {
+    color: hsla(120, 100%, 50%, 1);
+    text-shadow: 0px 0px 9.12281px hsla(120, 100%, 50%, .8),
+      0px 0px 54.7368px hsla(120, 100%, 50%, 1);
+  }
+  .delinquent-text {
     color: hsla(0, 100%, 50%, 1);
     text-shadow: 0px 0px 9.12281px hsla(0, 100%, 50%, .8),
       0px 0px 54.7368px hsla(0, 100%, 50%, 1);
-    filter: blur(0.03em);
+    animation: fade 1000ms infinite;
+  }
+  .times {
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+  }
+  .status {
+    text-align: center;
+  }
+  .ratio {
+    text-align: right;
+    width: 25%;
   }
 </style>
